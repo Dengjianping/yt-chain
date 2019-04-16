f2576e0b4ab89da58e4511ef295e31720dd30358
Dengjianping <djptux@gmail.com>
2019-04-16 22:18:27 +0800

    API changing

--- gpu_mining/src/lib.rs
+++ gpu_mining/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg(feature = "cuda")]
 
-use libc::{ c_char, c_int };
+use libc::{ c_char, c_int, c_uint, c_ulonglong };
 use std::ffi::{ CStr, CString };
 use std::ptr;
 
@@ -9,14 +9,15 @@ pub(crate) type Result<T> = std::result::Result<T, Box<dyn std::error::Error + S
 
 #[link(name="cuda_mining", kind="static")]
 extern {
-    fn MD5(a: *const c_char) -> *const c_char;
-    fn SHA256(a: *const c_char) -> *const c_char;
-    fn SHA384(a: *const c_char) -> *const c_char;
-    fn SHA512(a: *const c_char) -> *const c_char;
+    // fn MD5(a: *const c_char) -> *const c_char;
+    fn SHA256(prev_proof: c_uint, proof_of_work: *const c_char) -> c_uint;
+    fn SHA384(prev_proof: c_ulonglong, proof_of_work: *const c_char) -> c_ulonglong;
+    fn SHA512(prev_proof: c_ulonglong, proof_of_work: *const c_char) -> c_ulonglong;
     fn get_gpu_props() -> *const GPU_Props;
 }
 
 
+/*
 #[inline]
 // There's bug that generate wrong md5 value, under debug now.
 // pub fn cuda_md5<T: AsRef<str>>(t: T) -> Result<String> {
@@ -27,32 +28,27 @@ fn cuda_md5<T: AsRef<str>>(t: T) -> Result<String> {
     let str_slice = r_str.to_str()?; // &str
     Ok(str_slice.to_string())
 }
+*/
 
 #[inline]
-pub fn cuda_sha256<T: AsRef<str>>(t: T) -> Result<String> {
-    let c_str = CString::new(t.as_ref())?;
-    let r_ptr = unsafe { SHA256(c_str.as_ptr()) }; // c_char
-    let r_str: &CStr = unsafe { CStr::from_ptr(r_ptr) }; // &CStr
-    let str_slice = r_str.to_str()?; // &str
-    Ok(str_slice.to_string())
+pub fn cuda_sha256<T: AsRef<str>>(prev_proof: u64, proof_of_work: T) -> Result<u32> {
+    let c_str = CString::new(proof_of_work.as_ref())?;
+    let proof = unsafe { SHA256(prev_proof as c_uint, c_str.as_ptr()) };
+    Ok(proof)
 }
 
 #[inline]
-pub fn cuda_sha384<T: AsRef<str>>(t: T) -> Result<String> {
-    let c_str = CString::new(t.as_ref())?;
-    let r_ptr = unsafe { SHA384(c_str.as_ptr()) }; // c_char
-    let r_str: &CStr = unsafe { CStr::from_ptr(r_ptr) }; // &CStr
-    let str_slice = r_str.to_str()?; // &str
-    Ok(str_slice.to_string())
+pub fn cuda_sha384<T: AsRef<str>>(prev_proof: u64, proof_of_work: T) -> Result<u64> {
+    let c_str = CString::new(proof_of_work.as_ref())?;
+    let proof = unsafe { SHA384(prev_proof, c_str.as_ptr()) };
+    Ok(proof)
 }
 
 #[inline]
-pub fn cuda_sha512<T: AsRef<str>>(t: T) -> Result<String> {
-    let c_str = CString::new(t.as_ref())?;
-    let r_ptr = unsafe { SHA512(c_str.as_ptr()) }; // c_char
-    let r_str: &CStr = unsafe { CStr::from_ptr(r_ptr) }; // &CStr
-    let str_slice = r_str.to_str()?; // &str
-    Ok(str_slice.to_string())
+pub fn cuda_sha512<T: AsRef<str>>(prev_proof: u64, proof_of_work: T) -> Result<u64> {
+    let c_str = CString::new(proof_of_work.as_ref())?;
+    let proof = unsafe { SHA512(prev_proof, c_str.as_ptr()) };
+    Ok(proof)
 }
 
 #[inline]
@@ -64,6 +60,7 @@ pub fn gpu_properties() -> Result<GPU_Props> {
     Ok(props)
 }
 
+#[allow(non_snake_case)]
 #[repr(C)]
 #[derive(Clone, Debug)]
 pub struct GPU_Props {
